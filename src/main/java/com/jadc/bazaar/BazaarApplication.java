fc9458fc3e8071a8880cb275be98a7a4336230c6
ardiesan <113565882+ardiesan@users.noreply.github.com>
2022-09-19 09:27:03 +0800

    Merging PR before 'develop' branch (#8)
    Merging PR sent to branch 'main' before branch 'develop' was created.
    
    Co-authored-by: Cloyd-Anis <cloyd.anis@gmail.com>
    Co-authored-by: dr0psh0t <dagondondaryll@gmail.com>

--- src/main/java/com/jadc/bazaar/BazaarApplication.java
+++ src/main/java/com/jadc/bazaar/BazaarApplication.java
@@ -1,13 +1,26 @@
 package com.jadc.bazaar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class BazaarApplication {
+public class BazaarApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(BazaarApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BazaarApplication.class, args);
 	}
 
+	@Autowired
+	private Environment env;
+
+	@Override public void run(String... args) throws Exception {
+		logger.info("{}", env.getProperty("env.name"));
+	}
 }
