8310045c38edc433163f626735be0867fc8cf585
dr0psh0t <dagondondaryll@gmail.com>
2022-09-22 15:33:18 +0800

    [feature/deletes] added method for search and deletion

--- src/main/java/com/jadc/bazaar/service/CustomerService.java
+++ src/main/java/com/jadc/bazaar/service/CustomerService.java
@@ -2,6 +2,8 @@ package com.jadc.bazaar.service;
 
 import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+
 import com.jadc.bazaar.entity.Customer;
 
 public interface CustomerService {
@@ -9,4 +11,6 @@ public interface CustomerService {
 	Customer findById(int id);
 	Customer save(Customer customer);
 	void deleteById(int id);
+	void deleteSelectedRows(List<Integer> userIds);
+	List<Customer> searchByCompanyNameLike(@Param("keyword") String keyword);
 }
