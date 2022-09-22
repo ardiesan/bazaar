90e84ca914e6682cee7548bf69839dcf7c45138c
dr0psh0t <dagondondaryll@gmail.com>
2022-09-22 16:58:37 +0800

    [feature/deletes] removed method that deletes to the database

--- src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
+++ src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
@@ -2,10 +2,7 @@ package com.jadc.bazaar.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,9 +15,4 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 	@Query(nativeQuery = true, value = "SELECT * FROM customers WHERE company_name LIKE %:keyword%")
 	List<Customer> searchByCompanyNameLike(@Param("keyword") String keyword);
-
-	@Modifying
-	@Transactional
-	@Query(nativeQuery = true, value = "DELETE FROM customers WHERE id IN(?1)")
-	void deleteSelectedRows(List<Integer> userIds);
 }
