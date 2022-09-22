cde3f06169a0198a22c2d7de2061d07a8c3b13f9
dr0psh0t <dagondondaryll@gmail.com>
2022-09-22 15:32:44 +0800

    [feature/deletes] added methods for search and delete

--- src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
+++ src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
@@ -2,10 +2,25 @@ package com.jadc.bazaar.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.jadc.bazaar.entity.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-	public List<Customer> findByIsDeletedFalse();
+	List<Customer> findByIsDeletedFalse();
+
+	@Query(nativeQuery = true, value = "SELECT * FROM customers WHERE company_name LIKE %:keyword%")
+	List<Customer> searchByCompanyNameLike(@Param("keyword") String keyword);
+
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "DELETE FROM customers WHERE id IN(?1)")
+	void deleteSelectedRows(List<Integer> userIds);
 }
