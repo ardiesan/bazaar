96f0b9a99092fdba1f139a5cbaea2344aedd2543
jannielynacha <acha.jannielyn@gmail.com>
2022-09-22 10:00:13 +0800

    Add Customer entity

---
+++ src/main/java/com/jadc/bazaar/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.jadc.bazaar.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "customers")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "company_name")
+	private String companyName;
+
+	@Column(name = "records_used")
+	private int recordsUsed;
+
+	@Column(name = "contracted_records")
+	private int contractedRecords;
+
+	@Column(name = "created_at", updatable = false)
+	@CreatedDate
+	private Date createdAt;
+
+	@Column(name = "updated_at")
+	@LastModifiedDate
+	private Date updatedAt;
+
+	@Column(name = "is_deleted")
+	private boolean isDeleted;
+
+	@Column(name = "deleted_at")
+	private Date deletedAt;
+}
