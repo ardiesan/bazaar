0d1773b76f4c0f69f35a14c8c30d8a329ef0ea50
jannielynacha <acha.jannielyn@gmail.com>
2022-09-23 10:37:44 +0800

    Add search function, and change return type of findall to Page instead of List

--- src/main/java/com/jadc/bazaar/service/CustomerService.java
+++ src/main/java/com/jadc/bazaar/service/CustomerService.java
@@ -2,11 +2,15 @@ package com.jadc.bazaar.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.jadc.bazaar.entity.Customer;
 
 public interface CustomerService {
-	List<Customer> findAll();
+	Page<Customer> findAll(int offset);
+	Page<Customer> search(String companyName, int offset);
 	Customer findById(int id);
 	Customer save(Customer customer);
 	void deleteById(int id);
+	void deleteSelectedRows(List<Integer> userIds);
 }
