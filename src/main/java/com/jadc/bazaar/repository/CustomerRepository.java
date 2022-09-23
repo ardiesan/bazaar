f5e4eb5c8b0a22f8bc80ca366d2562f8062dcfed
jannielynacha <acha.jannielyn@gmail.com>
2022-09-23 10:35:09 +0800

    Update CustomerRepo to return Customer Page instead of List

--- src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
+++ src/main/java/com/jadc/bazaar/repository/CustomerRepository.java
@@ -1,11 +1,12 @@
 package com.jadc.bazaar.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jadc.bazaar.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-	public List<Customer> findByIsDeletedFalse();
+	Page<Customer> findByIsDeletedFalse(Pageable pageable);
+	Page<Customer> findByIsDeletedFalseAndCompanyNameContaining(String companyName, Pageable pageable);
 }
