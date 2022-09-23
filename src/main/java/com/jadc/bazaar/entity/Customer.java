f6a40ada6ad7c78d1ab8480f0e0b9a8ee0d58739
jannielynacha <acha.jannielyn@gmail.com>
2022-09-23 10:38:46 +0800

    Change audit meta type to LocalDateTime

--- src/main/java/com/jadc/bazaar/entity/Customer.java
+++ src/main/java/com/jadc/bazaar/entity/Customer.java
@@ -1,6 +1,6 @@
 package com.jadc.bazaar.entity;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,7 +26,7 @@ public class Customer {
 	@Column(name = "id")
 	private int id;
 
-	@Column(name = "company_name")
+	@Column(name = "company_name", length = 128)
 	private String companyName;
 
 	@Column(name = "records_used")
@@ -35,17 +35,18 @@ public class Customer {
 	@Column(name = "contracted_records")
 	private int contractedRecords;
 
-	@Column(name = "created_at", updatable = false)
-	@CreatedDate
-	private Date createdAt;
-
-	@Column(name = "updated_at")
-	@LastModifiedDate
-	private Date updatedAt;
-
 	@Column(name = "is_deleted")
 	private boolean isDeleted;
 
+	@Column(name = "created_at", updatable = false)
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+
 	@Column(name = "deleted_at")
-	private Date deletedAt;
+	private LocalDateTime deletedAt;
 }
