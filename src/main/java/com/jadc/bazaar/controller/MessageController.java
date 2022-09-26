19ece5ba2353c714cef1ef1c265503d86a204e6e
dr0psh0t <dagondondaryll@gmail.com>
2022-09-26 14:32:15 +0800

    BZR-13 controller for testing url pattern for locale language

---
+++ src/main/java/com/jadc/bazaar/controller/MessageController.java
@@ -0,0 +1,15 @@
+package com.jadc.bazaar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{lang}")
+public class MessageController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
