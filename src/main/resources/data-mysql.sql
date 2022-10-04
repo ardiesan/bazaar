6245c2c7b3b32314e186cdf2ac701980b0ee2c11
dr0psh0t <dagondondaryll@gmail.com>
2022-10-04 16:21:05 +0800

    BZR-47 initialization script at startup

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,6 +1,4 @@
-INSERT INTO `notification_categories` (`name`, `display_order`)
-VALUES
-('email', 3),
-('sms', 5),
-('twitter', 7),
-('line', 9);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('email', 3);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('sms', 5);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('twitter', 7);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('line', 9);
