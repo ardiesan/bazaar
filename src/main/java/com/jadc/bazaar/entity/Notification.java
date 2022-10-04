28820b65c0d5c7297a7c610baa3419178beb5b1a
dr0psh0t <dagondondaryll@gmail.com>
2022-10-04 16:21:16 +0800

    BZR-47 Notification entity

---
+++ src/main/java/com/jadc/bazaar/entity/Notification.java
@@ -0,0 +1,50 @@
+package com.jadc.bazaar.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "notifications")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Notification {
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "notification_category_id")
+	private NotificationCategory notificationCategory;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "title", length = 128)
+	private String title;
+
+	@Column(name = "updated_at")
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@Column(name = "created_at", updatable = false)
+	@CreatedDate
+	private LocalDateTime publicationDate;
+
+	@Column(name = "body", columnDefinition = "text")
+	private String body;
+}
