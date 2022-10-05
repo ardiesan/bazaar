80d0ad168c12648ffe56c163fe1537bbc2395038
CloydAnis <cloyd.anis@fullspeedtechnologies.com>
2022-10-05 09:50:43 +0800

    BZR-51 Added NotificationCategory entity with initial data from data-mysql.sql

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,4 @@
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('email', 3);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('sms', 5);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('twitter', 7);
+INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('line', 9);
