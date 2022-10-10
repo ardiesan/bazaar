7ee3482265271cc69538732ace243593299920eb
dr0psh0t <dagondondaryll@gmail.com>
2022-10-10 14:43:35 +0800

    BZR-48 2 routes for single display and default mapping to japanese

--- src/main/java/com/jadc/bazaar/controller/DefaultLanguageController.java
+++ src/main/java/com/jadc/bazaar/controller/DefaultLanguageController.java
@@ -6,23 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultLanguageController {
 
-	@GetMapping("/admin/account")
+	@GetMapping(value = {"/admin/account", "/"})
 	public String customers() {
 		return "redirect:/ja/admin/account";
 	}
 
-	@GetMapping("/")
-	public String root() {
-		return "redirect:/ja/admin/account";
-	}
-
-	@GetMapping("/admin/notification")
-	public String notificationImplicitIndexMapping() {
-		return "redirect:/en/admin/notification/index";
-	}
-
-	@GetMapping("/admin/notification/index")
-	public String notificationExplicitIndexMapping() {
-		return "redirect:/en/admin/notification/index";
+	@GetMapping(value = {"/admin/notification", "/admin/notification/index"})
+	public String notifications() {
+		return "redirect:/ja/admin/notification/index";
 	}
 }
