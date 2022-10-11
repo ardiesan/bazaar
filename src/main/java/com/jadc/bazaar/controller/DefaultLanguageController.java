d487611c9bc2e5e3e3dfa205c3a6aa6da129d30f
dr0psh0t <dagondondaryll@gmail.com>
2022-10-11 09:40:24 +0800

    BZR-48 2 routes. removed hardcoded locale.

--- src/main/java/com/jadc/bazaar/controller/DefaultLanguageController.java
+++ src/main/java/com/jadc/bazaar/controller/DefaultLanguageController.java
@@ -1,5 +1,7 @@
 package com.jadc.bazaar.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,12 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DefaultLanguageController {
 
 	@GetMapping(value = {"/admin/account", "/"})
-	public String customers() {
-		return "redirect:/ja/admin/account";
+	public String customers(Locale locale) {
+		String lang = locale.getLanguage();
+
+		return "redirect:/"+lang+"/admin/account";
 	}
 
 	@GetMapping(value = {"/admin/notification", "/admin/notification/index"})
-	public String notifications() {
-		return "redirect:/ja/admin/notification/index";
+	public String notifications(Locale locale) {
+		String lang = locale.getLanguage();
+
+		return "redirect:/"+lang+"/admin/notification/index";
 	}
 }
