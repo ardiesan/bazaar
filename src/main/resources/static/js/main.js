fd38fe69d57f231908088894adad3b6e2c8d1bb0
Jannielyn Acha <acha.jannielyn@gmail.com>
2022-10-12 11:09:17 +0800

    BZR-50 Notification i18n (#30)
    ## Overview
    - Internationalized Notifications module (en and ja)
    
    ## Change Summary
    - Added properties in the resource bundle for Notification
    internationalization
    - Internationalized the view templates
    - Fixed bugs on side navigation, pagination and modals
    
    ## JIRA Ticket
    https://ardie.atlassian.net/browse/BZR-50

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
-    $("#deleteButton").click(function() {
-        var $fields = $("#deleteForm").find('input[name="customers"]:checked');
-
-        if (!$fields.length) {
-            $('#errorDeleteModal').modal('show');
+    $("#deleteCustomerButton").click(function() {
+        var $customerFields = $("#deleteCustomerForm").find('input[name="customers"]:checked');
+        if (!$customerFields.length) {
+            $('#errorDeleteCustomerModal').modal('show');
             return false
         }
 
         return true;
     });
 
-    $("#deleteButtonNotification").click(function() {
-        var $fields = $("#deleteFormNotification").find('input[name="notifications"]:checked');
-
-        if (!$fields.length) {
-            $('#errorDeleteModalNotification').modal('show');
+    $("#deleteNotificationButton").click(function() {
+        var $notificationFields = $("#deleteNotificationForm").find('input[name="notifications"]:checked');
+        if (!$notificationFields.length) {
+            $('#errorDeleteNotificationModal').modal('show');
             return false
         }
 
         return true;
     });
 
-    $("#success-alert").delay(4000).slideUp(200, function() {
+    $(".alert-success").delay(4000).slideUp(200, function() {
         console.log("success closing");
         $(this).alert('close');
     });
 
-    $("#failure-alert").delay(4000).slideUp(200, function() {
+    $(".alert-failure").delay(4000).slideUp(200, function() {
         console.log("failure closing");
         $(this).alert('close');
     });
