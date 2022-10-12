a4d09b9401bac06778cf9a8e47079bc8de2321b1
dr0psh0t <dagondondaryll@gmail.com>
2022-10-12 17:29:03 +0800

    BZR-55 minified

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,4 +1,6 @@
-INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('email', 3);
-INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('sms', 5);
-INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('twitter', 7);
-INSERT INTO `bzrdb`.`notification_categories` (`name`, `display_order`) VALUES ('line', 9);
+INSERT INTO `notification_categories` (`name`, `display_order`)
+VALUES
+('email', 3),
+('sms', 5),
+('twitter', 7),
+('line', 9);
